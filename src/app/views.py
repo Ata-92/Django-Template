295c7f58f5376a52d8d451739f4e2b599aff917e
Ata-92 <atakal92@gmail.com>
2021-07-09 17:59:29 +0300

    Add an html file and a view

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
-def home_view(request):
+# def home_view(request):
     # print(request.META)
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+
+def home_view(request):
+    context = {
+        "first_name": "Ata",
+        "last_name": "Dev"
+    }
+    return render(request, "app/home.html", context)
